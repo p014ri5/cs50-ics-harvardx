f874732f48c824740c74a1ab46e5a03146ccbbac
p014ri5 <72374228+p014ri5@users.noreply.github.com>
2020-12-24 18:25:13 +0530

    Removed Todo comments

--- pset2/pset3/plurality.c
+++ pset2/pset3/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i].name) == 0)
@@ -81,7 +81,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
     
     int maxvotes = 0;
     
